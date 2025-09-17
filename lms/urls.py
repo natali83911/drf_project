dcba6e70ebed4b75227b1af66aa04b602657131f
Natalya Sorokina <natali83911@gmail.com>
2025-09-17 18:30:06 +0300

    2 added files

---
+++ lms/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import (
+    CourseViewSet,
+    LessonListCreateAPIView,
+    LessonRetrieveUpdateDestroyAPIView,
+)
+
+
+app_name = "lms"
+
+router = DefaultRouter()
+router.register(r"courses", CourseViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path("lessons/", LessonListCreateAPIView.as_view(), name="lesson-list-create"),
+    path(
+        "lessons/<int:pk>/",
+        LessonRetrieveUpdateDestroyAPIView.as_view(),
+        name="lesson-detail",
+    ),
+]
+
+urlpatterns += router.urls
