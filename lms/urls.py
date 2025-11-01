5d8ed7862d33d0044403b7349e016db46932d7b0
Natalya Sorokina <natali83911@gmail.com>
2025-11-01 18:05:57 +0300

    Test CI/CD after fixing SSH key

--- lms/urls.py
+++ lms/urls.py
@@ -1,9 +1,12 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import (CourseViewSet, LessonListCreateAPIView,
-                    LessonRetrieveUpdateDestroyAPIView,
-                    SubscriptionToggleAPIView)
+from .views import (
+    CourseViewSet,
+    LessonListCreateAPIView,
+    LessonRetrieveUpdateDestroyAPIView,
+    SubscriptionToggleAPIView,
+)
 
 app_name = "lms"
 
